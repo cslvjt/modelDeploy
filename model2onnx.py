8c3f9b22dea5e3c48308e54eacb52c486bb95210
cslvjt <lvjt@njust.edu.cn>
2024-03-16 20:13:18 +0800

    采用参数传递的形式，将模型转为onnx

--- model2onnx.py
+++ model2onnx.py
@@ -1,19 +1,32 @@
 import torch
 from SRCNN.srcnn_arch import load_net
+import argparse
 
-def model2onnx(onnx_path):
+def model2onnx(args):
     # TODO: 如何自定义适应输入分辨率
-    inputTensor = torch.randn(1, 3, 256, 256) 
-    net = load_net()
+    inputTensor = torch.randn(1, 3, args.h, args.w)
+
+    if args.model_name == "SRCNN":
+        net = load_net(args.weight_path)
+    else:
+        raise ValueError(f"{args.model_name} not find")
     with torch.no_grad():
         torch.onnx.export(
             net,
             inputTensor,
-            onnx_path,
+            args.onnx_path,
             opset_version=11, 
             input_names=['input'], 
             output_names=['output']
         )
 
 if __name__ == "__main__":
-    model2onnx("SRCNN/srcnn.onnx")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--h", type=int, default=256)
+    parser.add_argument("--w", type=int, default=256)
+    parser.add_argument("--weight_path", type=str)
+    parser.add_argument("--onnx_path", type=str)
+    parser.add_argument("--model_name", type=str)
+    args = parser.parse_args()
+    print(args)
+    model2onnx(args)
